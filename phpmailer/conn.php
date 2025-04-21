850f9b25ce6f6ae09858d953130f12b50a588c1b
diego <63115070+zarkius@users.noreply.github.com>
2025-04-21 18:25:51 +0200

    modified:   phpmailer/conn.php

--- phpmailer/conn.php
+++ phpmailer/conn.php
@@ -1,7 +1,16 @@
 <?php 
-$host="localhost:3306";
-$user="pruebas";
-$pass="1234";
+// Database connection file
+require_once __DIR__ . '../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+// Cargar las variables de entorno
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$host="$_ENV[DB_HOST]";
+$user="$_ENV[DB_USER]";
+$pass="$_ENV[DB_PASS]";
 $db="two_step_verification";
 
 $conn=mysqli_connect($host,$user,$pass,$db);
